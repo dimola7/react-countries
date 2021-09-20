01a6bd3e68d749cff09cb75954def75185b8d24c
dimola7 <didiogidan@gmail.com>
2021-09-20 10:57:00 +0100

    changes

---
+++ src/app/components/ui/organisms/Results.tsx
@@ -0,0 +1,11 @@
+import Cards from "../molecules/Cards";
+
+const Results = () => {
+  return (
+    <div style={{ marginTop: "20px" }}>
+      <Cards />
+    </div>
+  );
+};
+
+export default Results;
